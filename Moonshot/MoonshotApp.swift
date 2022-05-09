0a19461cafebf9790b9376aeb61acb2335ce96f2
Lucas Chae <hi@lucaschae.com>
2022-05-09 15:39:41 +0900

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Lucas Chae on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
